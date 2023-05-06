7abb0f58899640aa9bc9cbebec0b221dcdef835e
Enes T <125091714+enes-th@users.noreply.github.com>
2023-05-06 13:36:00 +0200

    Update Main.java

--- src/main/java/enes/plugin/moderation/Main.java
+++ src/main/java/enes/plugin/moderation/Main.java
@@ -10,6 +10,8 @@ import enes.plugin.moderation.events.PlayersFreeze;
 import enes.plugin.moderation.events.PlayersJoin;
 import enes.plugin.moderation.storage.Database;
 import enes.plugin.moderation.storage.cache.Data;
+import enes.plugin.moderation.utils.Players;
+import enes.plugin.moderation.utils.Reports;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,12 +21,19 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Main extends JavaPlugin {
-    private Database database;
+    private final Database database;
+    private final Players players;
+    private final Reports reports;
+
+    public Main() throws SQLException {
+        this.database = new Database(getDataFolder() + File.separator + "database.db");
+        this.players = new Players(database);
+        this.reports = new Reports(database);
+    }
 
     @Override
     public void onEnable() {
         try {
-            database = new Database(getDataFolder() + File.separator + "database.db");
             database.createTable();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,16 +42,16 @@ public class Main extends JavaPlugin {
             return;
         }
 
-        getCommand("report").setExecutor(new Report(database));
+        getCommand("report").setExecutor(new Report(reports, players));
         getCommand("moderator").setExecutor(new Moderator(this));
-        getCommand("report-check").setExecutor(new ReportCheck(database));
-        getCommand("player-check").setExecutor(new PlayerCheck(database));
+        getCommand("report-check").setExecutor(new ReportCheck(reports));
+        getCommand("player-check").setExecutor(new PlayerCheck(players));
 
         PluginManager pluginManager = getServer().getPluginManager();
-        pluginManager.registerEvents(new ModeratorsEnable(database, this), this);
+        pluginManager.registerEvents(new ModeratorsEnable(players, this), this);
         pluginManager.registerEvents(new ModeratorsJoin(), this);
         pluginManager.registerEvents(new PlayersFreeze(), this);
-        pluginManager.registerEvents(new PlayersJoin(database), this);
+        pluginManager.registerEvents(new PlayersJoin(players), this);
     }
 
     @Override
