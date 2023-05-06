0d55f0cfedea3b4758dc4b5ba145bcac54f14387
Enes T <125091714+enes-th@users.noreply.github.com>
2023-05-06 13:38:23 +0200

    Update PlayersJoin.java

--- src/main/java/enes/plugin/moderation/events/PlayersJoin.java
+++ src/main/java/enes/plugin/moderation/events/PlayersJoin.java
@@ -1,6 +1,5 @@
 package enes.plugin.moderation.events;
 
-import enes.plugin.moderation.storage.Database;
 import enes.plugin.moderation.utils.Players;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,8 +8,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayersJoin implements Listener {
     private final Players players;
 
-    public PlayersJoin(Database playerDatabase) {
-        this.players = Players.getInstance(playerDatabase);
+    public PlayersJoin(Players players) {
+        this.players = players;
     }
 
     @EventHandler
